a9248845aebdadf504e8ff70c679a7561ca95d39
My Name <someone@email.com>
2024-05-03 21:03:47 +0500

    portfolio

---
+++ app/components/AboutMe.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+
+
+const AboutMe = () => {
+    return (
+      <section className="about-me flex flex-col sm:flex-row items-center justify-center text-center sm:text-left py-24 mt-20"> {/* Adjusted margin from mt-12 to mt-20 */}
+        <div className="text-white max-w-lg">
+          <h2 className="mb-6 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-[#03d3fc]">About Me</span>
+          </h2>
+          <p className="text-lg sm:text-xl mb-8">
+            Hi, I'm Mubashir, a passionate web developer with a love for creating beautiful and functional websites. With a background in computer science and years of experience, I specialize in crafting responsive and user-friendly web applications.
+          </p>
+        </div>
+        <div className="mt-16 sm:mt-0 sm:ml-12"> {/* Adjusted margin-top from mt-10 to mt-16 */}
+          <Image src="/images/img.png" alt="Profile Image" width={200} height={200} className="rounded-full border-4 border-white" />
+        </div>
+      </section>
+    );
+  };
+  
+  export default AboutMe;
+  
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// Navbar.js
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="flex justify-between items-center">
+        <div className="text-white font-bold">
+          <Link href="/">Home</Link>
+        </div>
+        <div className="flex space-x-4">
+          <Link href="/about">About</Link>
+          <Link href="/skills">Skills</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
