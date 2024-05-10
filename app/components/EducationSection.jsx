1ce2d676f172597a48b3d9ae6d4917e9e17ea99f
My Name <someone@email.com>
2024-05-10 09:14:31 +0500

    portfolio

--- app/components/EducationSection.jsx
+++ app/components/EducationSection.jsx
@@ -1,5 +1,5 @@
-"use cleint"
 import React from 'react';
+import Image from "next/image";
 
 const EducationSection = () => {
   return (
@@ -10,21 +10,30 @@ const EducationSection = () => {
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         
-          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md">
+          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md flex flex-col items-center">
+            <div className="mb-4">
+              <Image src="/images/yul.jpg" alt="Profile Image" width={100} height={100} className="rounded-full align-center" />
+            </div>
             <h3 className="text-xl font-bold mb-4 text-[#fff]">Matric</h3>
             <p className="text-[#fff] mb-2">Uswa Public School Yultar</p>
             <p className="text-[#fff] mb-4">Science</p>
             <p className="text-[#fff]">2019-2021</p>
           </div>
 
-          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md">
+          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md flex flex-col items-center">
+            <div className="mb-4">
+              <Image src="/images/uswa2.png" alt="Profile Image" width={100} height={100} className="rounded-full align-center" />
+            </div>
             <h3 className="text-xl font-bold mb-4 text-[#fff]">Intermediate</h3>
             <p className="text-[#fff] mb-2">Uswa College Islamabad</p>
             <p className="text-[#fff] mb-4">Pre Medical</p>
             <p className="text-[#fff]">2021-2023</p>
           </div>
 
-          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md">
+          <div className="education-item bg-[#27b4e3] p-6 rounded-lg shadow-md flex flex-col items-center">
+            <div className="mb-4">
+              <Image src="/images/uswa2.png" alt="Profile Image" width={100} height={100} className="rounded-full align-center" />
+            </div>
             <h3 className="text-xl font-bold mb-4 text-white">Bachelor</h3>
             <p className="text-[#fff] mb-2">University ...</p>
             <p className="text-[#fff] mb-4">Field of Study</p>
